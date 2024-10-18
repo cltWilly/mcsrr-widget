edba9a31f0e73b51d5f5056bb2cc92ebad28fbad
cltWilly <vojtakrucky@gmail.com>
2024-10-18 19:36:15 +0200

    page Suspense edit

--- app/widget/now/page.jsx
+++ app/widget/now/page.jsx
@@ -1,127 +1,66 @@
 "use client";
 
+import { useState, useEffect, Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import { Widget } from "@/components/component/widget";
-import { useSearchParams } from 'next/navigation';
-import { useState, useEffect } from "react";
 
 async function fetchInitPlayer(playerName) {
   const res = await fetch(`https://mcsrranked.com/api/users/${playerName}`);
-    const result = await res.json();
-    console.log(result);
-    return result.data;
+  const result = await res.json();
+  console.log(result);
+  return result.data;
 }
 
 async function fetchPlayerMatches(playerUUID) {
-    const res = await fetch(`https://mcsrranked.com/api/users/${playerUUID}/matches?type=2&excludedecay=false`);
-    const result = await res.json();
-    console.log(result);
-    return result.data;
+  const res = await fetch(`https://mcsrranked.com/api/users/${playerUUID}/matches?type=2&excludedecay=false`);
+  const result = await res.json();
+  console.log(result);
+  return result.data;
 }
 
 function getCurrentTimestamp() {
-    const date = Math.floor(Date.now() / 1000);
-    // adjust the date +2 hours
-    console.log(date - 2 * 60 * 60);
-    return date;
-
-    // return Math.floor(Date.now() / 1000);
+  const date = Math.floor(Date.now() / 1000);
+  // adjust the date +2 hours
+  console.log(date - 2 * 60 * 60);
+  return date;
 }
 
 function getWinLoss(matches, playerUUID, startTimestamp) {
-    let wins = 0;
-    let losses = 0;
+  let wins = 0;
+  let losses = 0;
 
-    console.log("startTimestamp: " + startTimestamp + " in date format: " + new Date(startTimestamp * 1000));
-  
-    matches.forEach(match => {
-      if (match.date > startTimestamp) {
-        if (match.result.uuid === playerUUID) {
-          wins++;
-        } else {
-          losses++;
-        }
+  console.log("startTimestamp: " + startTimestamp + " in date format: " + new Date(startTimestamp * 1000));
+
+  matches.forEach(match => {
+    if (match.date > startTimestamp) {
+      if (match.result.uuid === playerUUID) {
+        wins++;
+      } else {
+        losses++;
       }
-    });
-  
-    return { wins, losses };
+    }
+  });
+
+  return { wins, losses };
 }
 
-
-/*
- "data": [
-    {
-      "id": 1317319,
-      "type": 2,
-      "category": "ANY",
-      "gameMode": "default",
-      "players": [
-        {
-          "uuid": "70eb9286e3e24153a8b37c8f884f1292",
-          "nickname": "7rowl",
-          "roleType": 0,
-          "eloRate": 1699,
-          "eloRank": 53,
-          "weeklyRaces": []
-        },
-        {
-          "uuid": "a81886565121479782d42408d94fe97d",
-          "nickname": "affordab1e",
-          "roleType": 1,
-          "eloRate": 1542,
-          "eloRank": 114,
-          "weeklyRaces": []
-        }
-      ],
-      "spectators": [],
-      "result": {
-        "uuid": "a81886565121479782d42408d94fe97d",
-        "time": 9936
-      },
-      "forfeited": true,
-      "decayed": false,
-      "rank": {
-        "season": null,
-        "allTime": null
-      },
-      "changes": [
-        {
-          "uuid": "a81886565121479782d42408d94fe97d",
-          "change": 26,
-          "eloRate": 1550
-        },
-        {
-          "uuid": "70eb9286e3e24153a8b37c8f884f1292",
-          "change": -26,
-          "eloRate": 1725
-        }
-      ],
-      "season": 6,
-      "date": 1729251645,
-      "seedType": "BURIED_TREASURE",
-      "bastionType": "TREASURE",
-      "tag": null
-    },
-*/
 function getEloPlusMinus(matches, playerUUID, startTimestamp) {
-    let eloPlusMinus = 0;
+  let eloPlusMinus = 0;
 
-    matches.forEach(match => {
-      if (match.date > startTimestamp) {
-        match.changes.forEach(change => {
-          if (change.uuid === playerUUID) {
-            eloPlusMinus += change.change;
-          }
-        });
-      }
-    });
-  
-    return eloPlusMinus;
-  }
+  matches.forEach(match => {
+    if (match.date > startTimestamp) {
+      match.changes.forEach(change => {
+        if (change.uuid === playerUUID) {
+          eloPlusMinus += change.change;
+        }
+      });
+    }
+  });
 
+  return eloPlusMinus;
+}
 
-
-
-export default function Page() {
+function WidgetPage() {
   const searchParams = useSearchParams();
   const player = searchParams.get('player'); 
 
@@ -154,49 +93,43 @@ export default function Page() {
     "1801-2000": "Diamond 3",
     "2001+": "Netherite 1",
   };
-    
 
-function getRank(elo) {
+  function getRank(elo) {
     console.log(elo);
 
     if (elo > 2001) {
-        return "Netherite 1";
+      return "Netherite 1";
     }
 
     for (const range in ranksTable) {
-        const [min, max] = range.split('-').map(Number);
-        if (elo >= min && elo <= max) {
+      const [min, max] = range.split('-').map(Number);
+      if (elo >= min && elo <= max) {
         return ranksTable[range];
-        }
+      }
     }
     return "Unranked";
-}
+  }
 
-
-useEffect(() => {
+  useEffect(() => {
     if (player) {
       let interval;
-  
+
       fetchInitPlayer(player).then((data) => {
         setPlayerUUID(data.uuid);
         setStartElo(data.eloRate);
         const initialTimestamp = getCurrentTimestamp();
         setInitialTimestamp(initialTimestamp);
-      
 
-  
         fetchPlayerMatches(data.uuid).then((matches) => {
-            console.log(initialTimestamp);
+          console.log(initialTimestamp);
           const { wins, losses } = getWinLoss(matches, data.uuid, initialTimestamp);
           setWinCount(wins);
           setLossCount(losses);
-
-          
         });
-  
+
         interval = setInterval(() => {
-            console.log("fetching new win loss data");
-            console.log("current timestamp: " + initialTimestamp);
+          console.log("fetching new win loss data");
+          console.log("current timestamp: " + initialTimestamp);
           fetchPlayerMatches(data.uuid).then((matches) => {
             const { wins, losses } = getWinLoss(matches, data.uuid, initialTimestamp);
             setWinCount(wins);
@@ -211,30 +144,37 @@ useEffect(() => {
             setCurrentElo(matches[0].players.find(player => player.uuid === data.uuid).eloRate);
           });
         }, 2 * 60 * 1000); // 2 minutes
-  
+
         console.log(data);
-  
+
         // if currentElo is null, set it to startElo
         if (currentElo === null) {
           setCurrentElo(data.eloRate);
           setEloPlusMinus(0);
         }
-  
+
         // set playerRank
         setPlayerRank(getRank(data.eloRate));
       });
-  
+
       return () => clearInterval(interval); // Clear interval on component unmount
     }
   }, [player]);
-  
-        
+
   return (
     <div className="relative min-h-screen">
       <div className="absolute top-0 left-0">
-        <Widget uuid= {playerUUID} elo={currentElo} eloPlusMinus={eloPlusMinus} playerRank={playerRank} startTimestamp={initialTimestamp} winCount={winCount} lossCount= {lossCount} /> 
+        <Widget uuid={playerUUID} elo={currentElo} eloPlusMinus={eloPlusMinus} playerRank={playerRank} startTimestamp={initialTimestamp} winCount={winCount} lossCount={lossCount} /> 
         {/* <a>playerName: {player}</a> */}
       </div>
     </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WidgetPage />
+    </Suspense>
   );
 }
