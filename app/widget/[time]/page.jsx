edddb49b592ae42beb3aee30474f06500c26245d
cltWilly <vojtakrucky@gmail.com>
2026-01-09 01:44:15 +0100

    coal image wasnt rendering fixed

--- app/widget/[time]/page.jsx
+++ app/widget/[time]/page.jsx
@@ -11,7 +11,7 @@ import {
   fetchAllMatches, 
   getEloPlusMinus 
 } from "@/lib/generatorUtils";
-import { calculateAverageTime, formatTime, calWinRate, rankIcons } from "@/lib/widgetUtils";
+import { calculateAverageTime, formatTime, calWinRate, rankIcons, ranksTable } from "@/lib/widgetUtils";
 
 function convertDateToTimestamp(date) {
   const decodedDate = decodeURIComponent(date);
@@ -69,40 +69,7 @@ function WidgetPage({ params }) {
   const [averageTime, setAverageTime] = useState(null);
   const [apiError, setApiError] = useState(null);
 
-  const ranksTable = {
-    "0-400": "Coal 1",
-    "401-500": "Coal 2",
-    "501-600": "Coal 3",
-    "601-700": "Iron 1",
-    "701-800": "Iron 2",
-    "801-900": "Iron 3",
-    "901-1000": "Gold 1",
-    "1001-1100": "Gold 2",
-    "1101-1200": "Gold 3",
-    "1201-1300": "Emerald 1",
-    "1301-1400": "Emerald 2",
-    "1401-1500": "Emerald 3",
-    "1501-1650": "Diamond 1",
-    "1651-1800": "Diamond 2",
-    "1801-2000": "Diamond 3",
-    "2001+": "Netherite 1",
-  };
 
-  const rankIcons = {
-    "Iron 1": "/iron.png",
-    "Iron 2": "/iron.png",
-    "Iron 3": "/iron.png",
-    "Gold 1": "/gold.png",
-    "Gold 2": "/gold.png",
-    "Gold 3": "/gold.png",
-    "Emerald 1": "/emerald.png",
-    "Emerald 2": "/emerald.png",
-    "Emerald 3": "/emerald.png",
-    "Diamond 1": "/diamond.png",
-    "Diamond 2": "/diamond.png",
-    "Diamond 3": "/diamond.png",
-    "Netherite 1": "/netherite.png",
-  };
 
   function getRank(elo) {
     console.log(elo);
